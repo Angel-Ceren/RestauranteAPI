99092f7c5c5cc7a5a38815ae424ae962bb2a789e
Angel Ceren <angel.ceren@outlook.com>
2023-09-25 19:56:14 -0600

    Se agregó la clase PedidoDTO

---
+++ DTOs/PedidoDTO.cs
@@ -0,0 +1,22 @@
+﻿using RestauranteAPI.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteAPI.DTOs
+{
+    public class PedidoDTO
+    {
+        public int Id { get; set; }
+
+        public DateTime FechaPedido { get; set; }
+
+        public string Estado { get; set; }
+
+        public decimal Total { get; set; }
+
+        // Llave foránea
+        public int UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
+    }
+}
