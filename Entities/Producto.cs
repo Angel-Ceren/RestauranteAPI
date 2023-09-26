3b211524a39b45d0fd7c31d9c53eddf90d0c5a5a
vladimirperez1106 <pl22002@esfe.agape.edu.sv>
2023-09-25 23:16:01 -0600

    Se crearon las propiedades de la tabla Productos

--- Entities/Producto.cs
+++ Entities/Producto.cs
@@ -2,5 +2,13 @@
 {
     public class Producto
     {
+        
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public string Categoria { get; set; }
+        public bool Disponible { get; set; }
+        public string Imagen { get; set; }
     }
 }
