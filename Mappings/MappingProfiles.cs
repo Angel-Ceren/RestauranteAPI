f1bdb7fa98a1773628981bf65db34aab9fcf176c
Luis Miranda <luisalcidesmiranda@yahoo.com>
2023-09-25 19:47:23 -0600

    - Se agregó la clase MappingProfiles.cs

---
+++ Mappings/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RestauranteAPI.DTOs;
+using RestauranteAPI.Entities;
+
+namespace RestauranteAPI.Mappings
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            // Entidad --> DTO
+            CreateMap<Usuario, UsuarioDTO>();
+
+            // DTO --> entidad
+            CreateMap<UsuarioDTO, Usuario>();
+
+        }
+    }
+}
