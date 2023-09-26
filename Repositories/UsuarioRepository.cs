779ad384b7fefc79c24baf83a330fe809e9c90fd
Luis Miranda <luisalcidesmiranda@yahoo.com>
2023-09-25 20:37:25 -0600

    - Se agregó la clase UsuarioRepository.cs a la carpeta Repositories

---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using RestauranteAPI.Context;
+using RestauranteAPI.DTOs;
+using RestauranteAPI.Entities;
+using RestauranteAPI.Repositories.Interfaces;
+
+namespace RestauranteAPI.Repositories
+{
+        public class UsuarioRepository : IUsuario
+        {
+            private readonly ApplicationDbContext _db;
+
+            private readonly IMapper _mapper;
+
+            public UsuarioRepository(ApplicationDbContext db, IMapper mapper)
+            {
+                _db = db;
+                _mapper = mapper;
+            }
+
+            public async Task<int> Crear(UsuarioDTO usuario)
+            {
+                var entidad = _mapper.Map<UsuarioDTO, Usuario>(usuario);
+                await _db.Usuarios.AddAsync(_mapper.Map<UsuarioDTO, Usuario>(usuario));
+
+                return await _db.SaveChangesAsync();
+            }
+        }
+  
+}
