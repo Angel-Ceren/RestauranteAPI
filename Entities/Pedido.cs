0aa74312236a2b1edce9c2b840f4847cf63cd79f
Angel Ceren <angel.ceren@outlook.com>
2023-09-25 19:46:07 -0600

    Se ingresaron las entidades de pedido

--- Entities/Pedido.cs
+++ Entities/Pedido.cs
@@ -1,6 +1,25 @@
-﻿namespace RestauranteAPI.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestauranteAPI.Entities
 {
     public class Pedido
     {
+        // Llave primaria
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime FechaPedido { get; set; }
+
+        public string Estado { get; set; }
+
+        public decimal Total { get; set; }
+
+        // Llave foránea
+        [ForeignKey(nameof(Usuario))]
+        public int UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
     }
 }
