3139bed2431a5de8e3c7f43b459146ef790c2bbb
Luis Miranda <luisalcidesmiranda@yahoo.com>
2023-09-27 17:55:55 -0600

    - Se agregó la clase UsuarioEndpoints.cs - ignorar otros cambios, solo se elimino una linea de codigo repetida que estaba causando problemas

--- Repositories/Interfaces/IUsuario.cs
+++ Repositories/Interfaces/IUsuario.cs
@@ -4,6 +4,9 @@ namespace RestauranteAPI.Repositories.Interfaces
 {
     public interface IUsuario
     {
+        
+        Task<int> Crear(UsuarioDTO usuario);
+
         public interface IUsuario
         {
             Task<int> Crear(UsuarioDTO usuario);
