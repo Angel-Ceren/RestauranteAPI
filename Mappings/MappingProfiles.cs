dc66bfc67444acd4338ea573f5c83f6458a1792e
Angel Ceren <angel.ceren@outlook.com>
2023-09-26 19:09:09 -0600

    Se agregó la conversion de entidad -> DTO de la tabla Pedido y viceversa

--- Mappings/MappingProfiles.cs
+++ Mappings/MappingProfiles.cs
@@ -9,10 +9,18 @@ namespace RestauranteAPI.Mappings
         public MappingProfiles()
         {
             // Entidad --> DTO
+            // Usuario
             CreateMap<Usuario, UsuarioDTO>();
+            // Pedido
+            CreateMap<Pedido, PedidoDTO>();
+            CreateMap<Pedido, GuardarPedido>();
 
             // DTO --> entidad
+            // Usuario
             CreateMap<UsuarioDTO, Usuario>();
+            // Pedido
+            CreateMap<PedidoDTO, Pedido>();
+            CreateMap<GuardarPedido, Pedido>();
 
         }
     }
