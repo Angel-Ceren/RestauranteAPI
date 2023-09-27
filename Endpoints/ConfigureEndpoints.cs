dfc886d71342eff208d9da768fb5f3df89903c27
vladimirperez1106 <pl22002@esfe.agape.edu.sv>
2023-09-26 23:23:55 -0600

    Se agrego la clase ConfigureEndpoints

---
+++ Endpoints/ConfigureEndpoints.cs
@@ -0,0 +1,12 @@
+﻿namespace RestauranteAPI.Endpoints
+{
+    public static class ConfigureEndpoints
+    {
+        public static void UseEndpoints(this WebApplication app)
+        {
+            //MUESTRA EL ENDPOINT EN EL NAVEGADOR lueg de añadir el endponits se escrie esta linea
+            //despues vamos a program para agregar la interfaz
+            ProductoEndpoints.Add(app);
+        }
+    }
+}
