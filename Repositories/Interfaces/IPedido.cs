23f1db0695501f1c8888a98d05d0cc63210d20a3
Angel Ceren <angel.ceren@outlook.com>
2023-09-27 23:00:26 -0600

    Se agregó la interfaz de Pedido

--- Repositories/Interfaces/IPedido.cs
+++ Repositories/Interfaces/IPedido.cs
@@ -1,6 +1,17 @@
-﻿namespace RestauranteAPI.Repositories.Interfaces
+﻿using RestauranteAPI.DTOs;
+
+namespace RestauranteAPI.Repositories.Interfaces
 {
     public interface IPedido
     {
+        Task<int> Crear(GuardarPedido pedido);
+
+        Task<ICollection<PedidoDTO>> Pedidos();
+
+        Task<PedidoDTO> Pedido(int id);
+
+        Task<int> Modificar(int id, GuardarPedido pedido);
+
+        Task<int> Eliminar(int id);
     }
 }
