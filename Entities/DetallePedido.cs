11362caa5ddec0b90269e088dc99667b1e171086
Angel Ceren <angel.ceren@outlook.com>
2023-09-28 23:11:24 -0600

    Se agregó la entidad de detalle pedido

--- Entities/DetallePedido.cs
+++ Entities/DetallePedido.cs
@@ -1,6 +1,31 @@
-﻿namespace RestauranteAPI.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteAPI.Entities
 {
     public class DetallePedido
     {
+        // Llave primaria
+        [Key]
+        public int Id { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal PrecioUnitario { get; set; }
+
+        public decimal Subtotal { get; set; }
+
+        // Llaves foráneas
+        [ForeignKey(nameof(Pedido))]
+
+        public int PedidoID { get; set; }
+
+        public Pedido Pedido { get; set; }
+
+        [ForeignKey(nameof(Producto))]
+
+        public int ProductoID { get; set; }
+
+        public Producto Producto { get; set; }
     }
 }
