c95990a03dd323d8bb5469ad2f16c25523e09a8d
Angel Ceren <angel.ceren@outlook.com>
2023-10-16 20:20:46 -0600

    Se agregaron las clases test

---
+++ RestauranteAPI.UnitTest/DetallePedidoTest.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RestauranteAPI.Context;
+using RestauranteAPI.Mappings;
+using RestauranteAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteAPI.UnitTest
+{
+    public class DetallePedidoTest
+    {
+        private readonly DetallePedidoRepository _detallePedidoRepository;
+        public DetallePedidoTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer("Data Source = DESKTOP-MQ3SAEG; Initial Catalog=ControlProductos; Integrated Security = True; Trust Server Certificate = True;").Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            var configurations = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfiles>();
+            });
+
+            var mapper = configurations.CreateMapper();
+
+            _detallePedidoRepository = new DetallePedidoRepository(dbContext, mapper);
+        }
+    }
+}
---
+++ RestauranteAPI.UnitTest/PedidoTest.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RestauranteAPI.Context;
+using RestauranteAPI.Mappings;
+using RestauranteAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteAPI.UnitTest
+{
+    public class PedidoTest
+    {
+        private readonly PedidoRepository _pedidoRepository;
+        public PedidoTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer("Data Source = DESKTOP-MQ3SAEG; Initial Catalog=ControlProductos; Integrated Security = True; Trust Server Certificate = True;").Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            var configurations = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfiles>();
+            });
+
+            var mapper = configurations.CreateMapper();
+
+            _pedidoRepository = new PedidoRepository(dbContext, mapper);
+        }
+    }
+}
